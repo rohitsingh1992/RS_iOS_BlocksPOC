ddfdc9c52b20f4c6d577916a0b2d64da36d1d653
rohitsingh1992 <rohit13singh@gmail.com>
2016-03-22 10:47:55 +0530

    second commit

---
+++ Blocks Full TutorialTests/Blocks_Full_TutorialTests.m
@@ -0,0 +1,34 @@
+//
+//  Blocks_Full_TutorialTests.m
+//  Blocks Full TutorialTests
+//
+//  Created by Rohit Singh on 01/11/14.
+//  Copyright (c) 2014 Rohit Singh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Blocks_Full_TutorialTests : XCTestCase
+
+@end
+
+@implementation Blocks_Full_TutorialTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
